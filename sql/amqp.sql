cde00ab41e02f1dfc376e17ccad2451bbf7a936f
marcello.ceschia <marcello.ceschia@users.sourceforge.net>
2012-05-28 12:05:24 +0200

    start with ssl implementation

--- sql/amqp.sql
+++ sql/amqp.sql
@@ -49,6 +49,13 @@ create table amqp.broker (
   vhost text,
   username text,
   password text,
+  requiressl boolean DEFAULT false,
+  verify_cert boolean DEFAULT true,
+  verify_cn boolean DEFAULT true,
+  cert text,
+  key text,
+  key_password character varying,
+  ca text,
   primary key(broker_id, host, port)
 );
 
