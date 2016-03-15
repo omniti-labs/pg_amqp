1290d7cfd4c7ba1e2d7a502708dcfeb53b308f3b
Keith Fiske <keith@keithf4.com>
2016-03-15 16:19:54 -0400

    v0.4.1 Clarify commit warning messages. See CHANGELOG for more info and the extension update file for how to install

---
+++ updates/amqp--0.4.0--0.4.1.sql
@@ -0,0 +1,3 @@
+-- NOTE: No changes to sql extension code contained in this update. Only C code has been patched, so a "make install" must be run to apply the bug fix. You must also still run "ALTER EXTENSION pg_ampq UPDATE' to update the extension version number in the database. 
+
+-- Clarify commit/rollback warning messages (Github Pull Request #15)
