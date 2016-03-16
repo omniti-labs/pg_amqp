7f4f3ece059b6916eeec53c3d329c3eed17279d5
Marcello Ceschia <marcello@ceschia.de>
2016-03-16 12:18:41 +0100

    update function definition as well

--- sql/amqp--0.4.2.sql
+++ sql/amqp--0.4.2.sql
@@ -75,12 +75,13 @@ CREATE FUNCTION @extschema@.publish(
     , reply_to varchar default null
     , correlation_id varchar default null
     , headers varchar[][2] default null
+    , properties varchar[][2] default null
 )
 RETURNS boolean AS 'pg_amqp.so', 'pg_amqp_publish'
 LANGUAGE C IMMUTABLE;
 
-COMMENT ON FUNCTION @extschema@.publish(integer, varchar, varchar, varchar, integer, varchar, varchar, varchar, varchar[][2]) IS
-'Publishes a message (broker_id, exchange, routing_key, message, delivery_mode, content_type, reply_to, correlation_id, headers). 
+COMMENT ON FUNCTION @extschema@.publish(integer, varchar, varchar, varchar, integer, varchar, varchar, varchar, varchar[][2], varchar[][2]) IS
+'Publishes a message (broker_id, exchange, routing_key, message, delivery_mode, content_type, reply_to, correlation_id, headers, properties). 
 The message will only be published if the containing PostgreSQL transaction successfully commits.  
 Under certain circumstances, the AMQP commit might fail.  In this case, a WARNING is emitted. 
 The last four parameters are optional and set the following message properties: 
